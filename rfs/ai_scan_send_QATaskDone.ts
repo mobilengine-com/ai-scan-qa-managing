42fbc64f698ed652b62e7c7972448c5fdf2bc9e1
Botond Portik-Bakai <botond.bakai@mobilengine.com>
2023-09-01 14:29:35 +0200

    JobId change and QATaskDone dacs handling
    JobId changed to requestFileId.
    Handling QATaskDone dacs response from gw.

---
+++ rfs/ai_scan_send_QATaskDone.ts
@@ -0,0 +1,16 @@
+//#server program ai_scan_send_QATaskDone for schedule 50 01 * * * first run at 2111-01-01 00:00
+//#using dacs QATaskDone;
+{
+    Log("params");
+    Log(params);
+    let dacs = messages.QATaskDone.New();
+    dacs.dnResponse.guid = params.guid;
+    dacs.dnResponse.requestFileId = params.requestFileId;
+    dacs.dnResponse.avgscoreMustHave = 0.0;
+    dacs.dnResponse.avgscoreOverall = 0.0;
+    dacs.dnResponse.accepted = params.accepted;
+    dacs.dnResponse.rejected = params.rejected;
+    dacs.dnResponse.handwritten = params.handwritten;
+    dacs.Send();
+}
+
