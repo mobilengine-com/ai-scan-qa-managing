af0cad8c8c641ac529a477633c811dc19af54748
Dauda Dani <daniel.dauda@bauapp.com>
2023-09-22 11:05:08 +0200

    BS-72
    BS-72
    CR: Feladat elvétel agenttől

--- rfs/ai_scan_coordinator_dashboard.ts
+++ rfs/ai_scan_coordinator_dashboard.ts
@@ -1,5 +1,8 @@
 //# server program ai_scan_coordinator_dashboard for form ai_scan_coordinator_dashboard
 //# using reftab ai_scan_user_language;
+//# using reftab ai_scan_job_inprogress;
+//# using reftab ai_scan_jobs;
+//# using reftab ai_scan_jobs_history;
 
 {
     let sbSave = form.sbtnSave;
@@ -44,6 +47,37 @@
                             lang: "hu_hu",
                             job_type: "ANOT"
                         });
+
+                        //If user has inprogress ANOT job and inprogress job language is hun then cancel the inprogress job
+                        let lstUserInprogressJob = db.ai_scan_job_inprogress.ReadFields({user_id: stUserId},["job_id"]);
+                        if(lstUserInprogressJob.Count() != 0)
+                        {
+                            let stJobId = lstUserInprogressJob.GetAt(0).job_id;
+                            let lstUserInprogressJobLang = db.ai_scan_jobs.ReadFields({id: stJobId, type: "ANOT", lang: "hu_hu"},["id"]);
+                            if(lstUserInprogressJobLang.Count() != 0)
+                            {
+                                // Update current job history with new online user
+                                let stCurrentJobId = stJobId;
+                                let stCurrentUser = stUserId;
+
+                                // Update the job in ai_scan_jobs table
+                                db.ai_scan_jobs.UpdateMany({
+                                    id : stCurrentJobId
+                                },{
+                                    status : "UNCHECKED",
+                                    current_user: null,
+                                    assigned: 0
+                                });
+                                // Delete the job in ai_scan_job_inprogress table
+                                db.ai_scan_job_inprogress.DeleteMany({job_id : stCurrentJobId, user_id : stCurrentUser});
+
+                                db.ai_scan_jobs_history.UpdateMany({
+                                    id: stCurrentJobId
+                                },{
+                                    job_assigned_status: ""
+                                });
+                            }
+                        }
                     }
                 }
 
@@ -75,6 +109,37 @@
                             lang: "eng_eng",
                             job_type: "ANOT"
                         });
+
+                        //If user has inprogress ANOT job and inprogress job language is eng then cancel the inprogress job
+                        let lstUserInprogressJob = db.ai_scan_job_inprogress.ReadFields({user_id: stUserId},["job_id"]);
+                        if(lstUserInprogressJob.Count() != 0)
+                        {
+                            let stJobId = lstUserInprogressJob.GetAt(0).job_id;
+                            let lstUserInprogressJobLang = db.ai_scan_jobs.ReadFields({id: stJobId, type: "ANOT", lang: "eng_eng"},["id"]);
+                            if(lstUserInprogressJobLang.Count() != 0)
+                            {
+                                // Update current job history with new online user
+                                let stCurrentJobId = stJobId;
+                                let stCurrentUser = stUserId;
+
+                                // Update the job in ai_scan_jobs table
+                                db.ai_scan_jobs.UpdateMany({
+                                    id : stCurrentJobId
+                                },{
+                                    status : "UNCHECKED",
+                                    current_user: null,
+                                    assigned: 0
+                                });
+                                // Delete the job in ai_scan_job_inprogress table
+                                db.ai_scan_job_inprogress.DeleteMany({job_id : stCurrentJobId, user_id : stCurrentUser});
+
+                                db.ai_scan_jobs_history.UpdateMany({
+                                    id: stCurrentJobId
+                                },{
+                                    job_assigned_status: ""
+                                });
+                            }
+                        }
                     }
                 }
 
@@ -106,6 +171,37 @@
                             lang: "pl_pl",
                             job_type: "ANOT"
                         });
+
+                        //If user has inprogress ANOT job and inprogress job language is pl then cancel the inprogress job
+                        let lstUserInprogressJob = db.ai_scan_job_inprogress.ReadFields({user_id: stUserId},["job_id"]);
+                        if(lstUserInprogressJob.Count() != 0)
+                        {
+                            let stJobId = lstUserInprogressJob.GetAt(0).job_id;
+                            let lstUserInprogressJobLang = db.ai_scan_jobs.ReadFields({id: stJobId, type: "ANOT", lang: "pl_pl"},["id"]);
+                            if(lstUserInprogressJobLang.Count() != 0)
+                            {
+                                // Update current job history with new online user
+                                let stCurrentJobId = stJobId;
+                                let stCurrentUser = stUserId;
+
+                                // Update the job in ai_scan_jobs table
+                                db.ai_scan_jobs.UpdateMany({
+                                    id : stCurrentJobId
+                                },{
+                                    status : "UNCHECKED",
+                                    current_user: null,
+                                    assigned: 0
+                                });
+                                // Delete the job in ai_scan_job_inprogress table
+                                db.ai_scan_job_inprogress.DeleteMany({job_id : stCurrentJobId, user_id : stCurrentUser});
+
+                                db.ai_scan_jobs_history.UpdateMany({
+                                    id: stCurrentJobId
+                                },{
+                                    job_assigned_status: ""
+                                });
+                            }
+                        }
                     }
                 }
             }
@@ -146,6 +242,37 @@
                             lang: "hu_hu",
                             job_type: "QA"
                         });
+
+                        //If user has inprogress QA job and inprogress job language is hun then cancel the inprogress job
+                        let lstUserInprogressJob = db.ai_scan_job_inprogress.ReadFields({user_id: stUserId},["job_id"]);
+                        if(lstUserInprogressJob.Count() != 0)
+                        {
+                            let stJobId = lstUserInprogressJob.GetAt(0).job_id;
+                            let lstUserInprogressJobLang = db.ai_scan_jobs.ReadFields({id: stJobId, type: "QA", lang: "hu_hu"},["id"]);
+                            if(lstUserInprogressJobLang.Count() != 0)
+                            {
+                                // Update current job history with new online user
+                                let stCurrentJobId = stJobId;
+                                let stCurrentUser = stUserId;
+
+                                // Update the job in ai_scan_jobs table
+                                db.ai_scan_jobs.UpdateMany({
+                                    id : stCurrentJobId
+                                },{
+                                    status : "UNCHECKED",
+                                    current_user: null,
+                                    assigned: 0
+                                });
+                                // Delete the job in ai_scan_job_inprogress table
+                                db.ai_scan_job_inprogress.DeleteMany({job_id : stCurrentJobId, user_id : stCurrentUser});
+
+                                db.ai_scan_jobs_history.UpdateMany({
+                                    id: stCurrentJobId
+                                },{
+                                    job_assigned_status: ""
+                                });
+                            }
+                        }
                     }
                 }
 
@@ -177,6 +304,37 @@
                             lang: "eng_eng",
                             job_type: "QA"
                         });
+
+                        //If user has inprogress QA job and inprogress job language is eng then cancel the inprogress job
+                        let lstUserInprogressJob = db.ai_scan_job_inprogress.ReadFields({user_id: stUserId},["job_id"]);
+                        if(lstUserInprogressJob.Count() != 0)
+                        {
+                            let stJobId = lstUserInprogressJob.GetAt(0).job_id;
+                            let lstUserInprogressJobLang = db.ai_scan_jobs.ReadFields({id: stJobId, type: "QA", lang: "eng_eng"},["id"]);
+                            if(lstUserInprogressJobLang.Count() != 0)
+                            {
+                                // Update current job history with new online user
+                                let stCurrentJobId = stJobId;
+                                let stCurrentUser = stUserId;
+
+                                // Update the job in ai_scan_jobs table
+                                db.ai_scan_jobs.UpdateMany({
+                                    id : stCurrentJobId
+                                },{
+                                    status : "UNCHECKED",
+                                    current_user: null,
+                                    assigned: 0
+                                });
+                                // Delete the job in ai_scan_job_inprogress table
+                                db.ai_scan_job_inprogress.DeleteMany({job_id : stCurrentJobId, user_id : stCurrentUser});
+
+                                db.ai_scan_jobs_history.UpdateMany({
+                                    id: stCurrentJobId
+                                },{
+                                    job_assigned_status: ""
+                                });
+                            }
+                        }
                     }
                 }
 
@@ -208,6 +366,37 @@
                             lang: "pl_pl",
                             job_type: "QA"
                         });
+
+                        //If user has inprogress QA job and inprogress job language is pl then cancel the inprogress job
+                        let lstUserInprogressJob = db.ai_scan_job_inprogress.ReadFields({user_id: stUserId},["job_id"]);
+                        if(lstUserInprogressJob.Count() != 0)
+                        {
+                            let stJobId = lstUserInprogressJob.GetAt(0).job_id;
+                            let lstUserInprogressJobLang = db.ai_scan_jobs.ReadFields({id: stJobId, type: "QA", lang: "pl_pl"},["id"]);
+                            if(lstUserInprogressJobLang.Count() != 0)
+                            {
+                                // Update current job history with new online user
+                                let stCurrentJobId = stJobId;
+                                let stCurrentUser = stUserId;
+
+                                // Update the job in ai_scan_jobs table
+                                db.ai_scan_jobs.UpdateMany({
+                                    id : stCurrentJobId
+                                },{
+                                    status : "UNCHECKED",
+                                    current_user: null,
+                                    assigned: 0
+                                });
+                                // Delete the job in ai_scan_job_inprogress table
+                                db.ai_scan_job_inprogress.DeleteMany({job_id : stCurrentJobId, user_id : stCurrentUser});
+
+                                db.ai_scan_jobs_history.UpdateMany({
+                                    id: stCurrentJobId
+                                },{
+                                    job_assigned_status: ""
+                                });
+                            }
+                        }
                     }
                 }
             }
