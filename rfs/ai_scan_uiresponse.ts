83a79a29e2f22cecae222cd972aea1f347da8627
Dauda Dani <daniel.dauda@bauapp.com>
2023-09-25 10:16:01 +0200

    BS-81
    BS-81
    Nem tudok törölni felhasználót

--- rfs/ai_scan_uiresponse.ts
+++ rfs/ai_scan_uiresponse.ts
@@ -6,6 +6,7 @@
     let stDacsMsgGuid = dacs.Response.MessageGuid;
 	let stDacsResultCode = dacs.Response.ResultCode;
 	let stDacsMessage = dacs.Response.Message;
+    let stDacsResult = null;
 
     let logRow = null;
 
@@ -18,7 +19,10 @@
 
     if(logRow.operation == "createuser")
     {
-        let stDacsResult = dacs.Response.Results.Result[0].Items[0].value;
+        if (dacs.Response.Results != null && dacs.Response.Results.Result.Count() == 1 && dacs.Response.Results.Result[0].Items.Count() == 1)
+        {
+            stDacsResult = dacs.Response.Results.Result[0].Items[0].value;
+        }
 
         logRow.state			= "succeeded";
         logRow.processed		= dtl.Now().DtlToDtdb();
@@ -26,9 +30,20 @@
         logRow.result_message	= stDacsMessage;
         logRow.result			= stDacsResult;
 
-        db.ai_scan_um_log.UpdateMany({msg_guid : stDacsMsgGuid},logRow);
+        if(dacs.Response.Results == null || logRow.result_code != "0")
+        {
+            db.ai_scan_user.DeleteMany({id : logRow.munkas_id});
 
-        db.ai_scan_user.UpdateMany({id: logRow.munkas_id},{user_id : logRow.result});
+            db.ai_scan_user.UpdateMany({msg_guid : logRow.msg_guid},logRow);
+            
+            Log("ERROR! User not created! Go to ai_scan_um_log reftab for more information!");
+        }
+        else
+        {
+            db.ai_scan_um_log.UpdateMany({msg_guid : stDacsMsgGuid},logRow);
+
+            db.ai_scan_user.UpdateMany({id: logRow.munkas_id},{user_id : logRow.result});
+        }
     }
 
     if(logRow.operation == "alteruser")
@@ -38,7 +53,10 @@
         logRow.result_code		= stDacsResultCode;
         logRow.result_message	= stDacsMessage;
 
-        db.ai_scan_um_log.UpdateMany({msg_guid : stDacsMsgGuid},logRow);
+        if(logRow.result_code == "0")
+        {
+            db.ai_scan_um_log.UpdateMany({msg_guid : stDacsMsgGuid},logRow);
+        }
     }
 
     if(logRow.operation == "deleteuser")
