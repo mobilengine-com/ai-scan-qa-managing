d73df04778ded8649776c86c741973c7ef811e2f
Dauda Dani <daniel.dauda@bauapp.com>
2023-11-29 17:09:31 +0100

    BS-155
    BS-155
    Failed RFS - ai_scan_NewQATask.ts(36,26): Unique constraint violation

--- rfs/ai_scan_NewQATask.ts
+++ rfs/ai_scan_NewQATask.ts
@@ -12,113 +12,126 @@
     let stJobId = dacs.qaTask.requestFileId1;
     let stJobId2 = dacs.qaTask.requestFileId2;
 
-    let stDefaultAIUser = db.ai_scan_settings.ReadFields({name: "ai_default_user"},["value"]).SingleOrDefault().value;
+    // If requestFileId1 and requestFileId2 exist in ai_scan_jobs reftab
 
-    if(stDefaultAIUser == "" || stDefaultAIUser == null)
+    let lstJobsExist = db.ai_scan_jobs.ReadFields({id: [stJobId,stJobId2]},["delivery_note_id"]);
+
+    if(lstJobsExist.Count() == 0)
     {
-        Log("No Default AI User in ai_scan_settings table!");
-        stDefaultAIUser = "";
-    }
+        // Set default user if exist in ai_scan_settings reftab
 
-    // Get supplier id if exist
-    let stSupplierID = null;
-    if(dacs.qaTask.supplierId != null || dacs.qaTask.supplierId != "")
+        let stDefaultAIUser = db.ai_scan_settings.ReadFields({name: "ai_default_user"},["value"]).SingleOrDefault().value;
+
+        if(stDefaultAIUser == "" || stDefaultAIUser == null)
+        {
+            Log("No Default AI User in ai_scan_settings table!");
+            stDefaultAIUser = "";
+        }
+
+        // Get supplier id if exist
+        let stSupplierID = null;
+        if(dacs.qaTask.supplierId != null || dacs.qaTask.supplierId != "")
+        {
+            stSupplierID = dacs.qaTask.supplierId;
+        }
+
+        //DEBUG Log (ANOT Job1 created)
+        Log("DEBUG");
+        Log(stJobId);
+        Log("ANOT Job1 created  / status -> UNCHECKED");
+
+        // Creat first job
+        db.ai_scan_jobs.Insert({
+            id: stJobId,
+            type: "ANOT",
+            status: "UNCHECKED",
+            supplier_id: stSupplierID,
+            lang: dacs.qaTask.lang,
+            current_user: null,
+            delivery_note_id: dacs.qaTask.scanId,
+            delivery_note_work_start_date : dacs.qaTask.dtlCreated.DtlToDtdb(),
+            create_date: dtl.Now().DtlToDtdb(),
+            delay_time: null,
+            job_id_2: stJobId2,
+            job_id_3: null,
+            job_url: dacs.qaTask.aiUrl1,
+            assigned: 0
+        });
+
+        // Creat first job history
+        db.ai_scan_jobs_history.Insert({
+            id: stJobId,
+            users: null
+        });
+
+        // Create delivery_note for first job
+        db.ai_scan_delivery_note_job.Insert({
+            delivery_note_id: dacs.qaTask.scanId,
+            job_id: stJobId,
+            status: "waiting",
+            avg_score_must_have: 0.0,
+            avg_score_overall: 0.0,
+            fileref_pdf: dacs.qaTask.mediaIdPdf == null ? null : fileref.New(dacs.qaTask.mediaIdPdf, 0)
+        });
+
+        //Add default user from ANNOT first job on AI page    
+        let dacs2 = messages.AssignAITask.New();
+        dacs2.assignTask.scanId = dacs.qaTask.scanId;
+        dacs2.assignTask.requestFileId = stJobId;
+        dacs2.assignTask.agent = stDefaultAIUser;
+        //dacs2.assignTask.agent = "botond.bakai@mobilengine.com";
+        dacs2.Send();
+
+        //DEBUG Log (ANOT Job2 created)
+        Log("DEBUG");
+        Log(stJobId2);
+        Log("ANOT Job2 created  / status -> UNCHECKED");
+
+        // Creat second job
+        db.ai_scan_jobs.Insert({
+            id: stJobId2,
+            type: "ANOT",
+            status: "UNCHECKED",
+            supplier_id: stSupplierID,
+            lang: dacs.qaTask.lang,
+            current_user: null,
+            delivery_note_id: dacs.qaTask.scanId,
+            delivery_note_work_start_date : dacs.qaTask.dtlCreated.DtlToDtdb(),
+            create_date: dtl.Now().DtlToDtdb(),
+            delay_time: null,
+            job_id_2: stJobId,
+            job_id_3: null,
+            job_url: dacs.qaTask.aiUrl2,
+            assigned: 0
+        });
+
+        // Create second job history
+        db.ai_scan_jobs_history.Insert({
+            id: stJobId2,
+            users: null
+        });
+
+        // Create delivery_note for second job
+        db.ai_scan_delivery_note_job.Insert({
+            delivery_note_id: dacs.qaTask.scanId,
+            job_id: stJobId2,
+            status: "waiting",
+            avg_score_must_have: 0.0,
+            avg_score_overall: 0.0,
+            fileref_pdf: dacs.qaTask.mediaIdPdf == null ? null : fileref.New(dacs.qaTask.mediaIdPdf, 0)
+        });
+
+        //Add default user from ANNOT second job on AI page
+        
+        let dacs3 = messages.AssignAITask.New();
+        dacs3.assignTask.scanId = dacs.qaTask.scanId;
+        dacs3.assignTask.requestFileId = stJobId2;
+        dacs3.assignTask.agent = stDefaultAIUser;
+        //dacs3.assignTask.agent = "botond.bakai@mobilengine.com";
+        dacs3.Send();
+    }
+    else
     {
-        stSupplierID = dacs.qaTask.supplierId;
+        Log("Annotation jobs exist in DM company!");
     }
-
-    //DEBUG Log (ANOT Job1 created)
-    Log("DEBUG");
-    Log(stJobId);
-    Log("ANOT Job1 created  / status -> UNCHECKED");
-
-    // Creat first job
-    db.ai_scan_jobs.Insert({
-        id: stJobId,
-        type: "ANOT",
-        status: "UNCHECKED",
-        supplier_id: stSupplierID,
-        lang: dacs.qaTask.lang,
-        current_user: null,
-        delivery_note_id: dacs.qaTask.scanId,
-        delivery_note_work_start_date : dacs.qaTask.dtlCreated.DtlToDtdb(),
-        create_date: dtl.Now().DtlToDtdb(),
-        delay_time: null,
-        job_id_2: stJobId2,
-        job_id_3: null,
-        job_url: dacs.qaTask.aiUrl1,
-        assigned: 0
-    });
-
-    // Creat first job history
-    db.ai_scan_jobs_history.Insert({
-        id: stJobId,
-        users: null
-    });
-
-    // Create delivery_note for first job
-    db.ai_scan_delivery_note_job.Insert({
-        delivery_note_id: dacs.qaTask.scanId,
-        job_id: stJobId,
-        status: "waiting",
-        avg_score_must_have: 0.0,
-        avg_score_overall: 0.0,
-        fileref_pdf: dacs.qaTask.mediaIdPdf == null ? null : fileref.New(dacs.qaTask.mediaIdPdf, 0)
-    });
-
-    //Add default user from ANNOT first job on AI page    
-    let dacs2 = messages.AssignAITask.New();
-    dacs2.assignTask.scanId = dacs.qaTask.scanId;
-    dacs2.assignTask.requestFileId = stJobId;
-    dacs2.assignTask.agent = stDefaultAIUser;
-    //dacs2.assignTask.agent = "botond.bakai@mobilengine.com";
-    dacs2.Send();
-
-    //DEBUG Log (ANOT Job2 created)
-    Log("DEBUG");
-    Log(stJobId2);
-    Log("ANOT Job2 created  / status -> UNCHECKED");
-
-    // Creat second job
-    db.ai_scan_jobs.Insert({
-        id: stJobId2,
-        type: "ANOT",
-        status: "UNCHECKED",
-        supplier_id: stSupplierID,
-        lang: dacs.qaTask.lang,
-        current_user: null,
-        delivery_note_id: dacs.qaTask.scanId,
-        delivery_note_work_start_date : dacs.qaTask.dtlCreated.DtlToDtdb(),
-        create_date: dtl.Now().DtlToDtdb(),
-        delay_time: null,
-        job_id_2: stJobId,
-        job_id_3: null,
-        job_url: dacs.qaTask.aiUrl2,
-        assigned: 0
-    });
-
-    // Create second job history
-    db.ai_scan_jobs_history.Insert({
-        id: stJobId2,
-        users: null
-    });
-
-    // Create delivery_note for second job
-    db.ai_scan_delivery_note_job.Insert({
-        delivery_note_id: dacs.qaTask.scanId,
-        job_id: stJobId2,
-        status: "waiting",
-        avg_score_must_have: 0.0,
-        avg_score_overall: 0.0,
-        fileref_pdf: dacs.qaTask.mediaIdPdf == null ? null : fileref.New(dacs.qaTask.mediaIdPdf, 0)
-    });
-
-    //Add default user from ANNOT second job on AI page
-    
-    let dacs3 = messages.AssignAITask.New();
-    dacs3.assignTask.scanId = dacs.qaTask.scanId;
-    dacs3.assignTask.requestFileId = stJobId2;
-    dacs3.assignTask.agent = stDefaultAIUser;
-    //dacs3.assignTask.agent = "botond.bakai@mobilengine.com";
-    dacs3.Send();
 }
